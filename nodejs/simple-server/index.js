7a08be17e840282fea903b363514018a1d1b16bf
Augusto Altman <augusto.altman@gmail.com>
2020-05-02 18:42:32 +1200

    add nodemon

--- nodejs/simple-server/index.js
+++ nodejs/simple-server/index.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
   res.send("hello world");
 });
 
+app.get("/api/courses", (req, res) => {
+  res.send([23, 4, 55]);
+});
+
 app.listen(8080, () => {
   console.log("server started");
 });
