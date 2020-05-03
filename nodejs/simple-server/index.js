8bb7721a35ecaa943bba9f33bdbc7eba9482c606
Augusto Altman <augusto.altman@gmail.com>
2020-05-03 16:06:14 +1200

    better tests

--- nodejs/simple-server/index.js
+++ nodejs/simple-server/index.js
@@ -5,6 +5,11 @@ const homeApi = require("./routes/home");
 const app = express();
 const port = process.env.PORT || 8080;
 
+const foo = { id: 1, name: "uno", opt: 22 };
+const bar = { id: 2, name: "dos", josh: 1 };
+
+console.table([foo, bar]);
+
 app.use(express.json());
 app.use("/api/courses", coursesApi);
 app.use("/", homeApi);
--- nodejs/simple-server/index.test.js
+++ nodejs/simple-server/index.test.js
@@ -1,5 +1,25 @@
+const func = (arg) => {
+  if (arg === 1) {
+    throw new Error("problem");
+  }
+  return ++arg;
+};
+
 describe("index", () => {
-  test("first", () => {
-    expect(3).toBe(3);
+  it("first", () => {
+    const obj1 = { id: 1, name: "as", la: 1 };
+    const obj2 = { id: 1, name: "as" };
+    expect(obj1).toMatchObject(obj2);
+  });
+
+  describe("func", () => {
+    it("second", () => {
+      expect(() => func(1)).toThrow();
+    });
+
+    it("third", () => {
+      const res = func(2);
+      expect(res).toBe(3);
+    });
   });
 });
